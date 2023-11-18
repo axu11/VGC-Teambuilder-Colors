28638059a458aaadb1eb01bc272e34817c92791f
axu11 <axu11@ucsc.edu>
2023-11-17 21:04:49 -0800

    it works when you click the button

--- content.js
+++ content.js
@@ -1,35 +1,47 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.action === 'changeColor') {
-        console.log("clicked");
-        // Change the color of the first paragraph on the page to red
-        var statsList = getStatsList();
+  function(request, sender, sendResponse) {
+    if (request.action === 'changeColor') {
+      console.log("clicked");
+      // Change the color of the first paragraph on the page to red
+      var statsList = getStatsList();
+      if(statsList) {
         var hueList = getNewHueList(statsList);
-        var statBars = document.querySelector('.graphcol');
-        if (statBars) {
-          for(var i = 0; i < statBars.length; i++) {
-            var hue = hueList[i];
-            statBars.item(i).style.background = `hsl(${hue}, 85%, 45%`;
+        var graphColEl = document.querySelector("#room-teambuilder > div > div.teambuilder-results > div.statform > div.col.graphcol");
+        if (graphColEl) {
+          for(var i = 2; i < 8; i++) {
+            var hue = hueList[i-2];
+            var graphSpan = graphColEl.querySelector(`div:nth-child(${i}) > em > span`);
+            if(graphSpan) {
+              graphSpan.style.background = `hsl(${hue}, 85%, 45%`;
+            } else {
+              console.log("cant get individual graph");
+            }
           }
         }
+      } else {
+        console.log("cant get graphCol");
       }
     }
-  );
+  }
+);
 
 function getStatsList() {
-    var statsCol = document.querySelector("#room-teambuilder > div > div.teambuilder-results > div.statform > div.col.statscol");
-    if(statsCol) {
-      var statsList = [];
-      // for(var i = 0; i < statsCol.length; i++) {
-      //     statsList.push(statsCol.item(i));
-      // }
-      statsList.push(document.querySelector("#room-teambuilder > div > div.teambuilder-results > div.statform > div.col.statscol > div:nth-child(2) > b"))
-      console.log("stats: " + statsList);
-      return statsList;
+  var statsList = [];
+  var statsColEl = document.querySelector("#room-teambuilder > div > div.teambuilder-results > div.statform > div.col.statscol")
+  if(statsColEl) {
+    for(var i = 2; i < 8; i++) {
+      var statText = statsColEl.querySelector(`div:nth-child(${i}) > b`).innerText;
+      if(statText) {
+        statsList.push(statText);
+      } else {
+        console.log("cant get individual stat");
+      }
     }
-    else {
-      console.log("cant get statsCol");
-    }
+    console.log("stats: " + statsList);
+    return statsList;
+  } else {
+    console.log("cant get statscol");
+  }
 }
 
 function getNewHueList(statsList) {
@@ -37,8 +49,10 @@ function getNewHueList(statsList) {
   for(var i = 0; i < statsList.length; i++) {
     hueList.push(statToHue(statsList[i]));
   }
+  console.log("hues: " + hueList);
+  return hueList;
 }
 
 function statToHue(stat) {
-    return Int((stat*0.498263) - 0.673852);
+    return (stat*0.498263) - 0.673852;
 }
