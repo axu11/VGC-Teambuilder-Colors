7c31bac4614f15ffc7b73830ccefcb0764970de3
axu11 <axu11@ucsc.edu>
2023-11-17 22:24:39 -0800

    always check for mutations and update

--- content.js
+++ content.js
@@ -1,33 +1,35 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.action === 'changeColor') {
-      console.log("clicked");
-      // Change the color of the first paragraph on the page to red
-      var statsList = getStatsList();
-      if(statsList) {
-        var hueList = getNewHueList(statsList);
-        var graphColEl = document.querySelector("#room-teambuilder > div > div.teambuilder-results > div.statform > div.col.graphcol");
-        if (graphColEl) {
-          for(var i = 2; i < 8; i++) {
-            var hue = hueList[i-2];
-            var graphSpan = graphColEl.querySelector(`div:nth-child(${i}) > em > span`);
-            if(graphSpan) {
-              graphSpan.style.background = `hsl(${hue}, 85%, 45%`;
-            } else {
-              console.log("cant get individual graph");
-            }
-          }
-        }
-      } else {
-        console.log("cant get graphCol");
-      }
-    }
-  }
-);
+var observer = new MutationObserver(function(mutations) {
+  console.log("in here");
+  mutations.forEach(function(mutation) {
+     // Log mutation details
+    //  console.log(mutation);
+     var statsList = getStatsList();
+     if(statsList) {
+       var hueList = getNewHueList(statsList);
+       var graphColEl = document.querySelector("#room-teambuilder > div > div.teambuilder-results > div.statform > div.col.graphcol");
+       if (graphColEl) {
+         for(var i = 2; i < 8; i++) {
+           var hue = hueList[i-2];
+           var graphSpan = graphColEl.querySelector(`div:nth-child(${i}) > em > span`);
+           if(graphSpan) {
+             graphSpan.style.background = `hsl(${hue}, 85%, 45%`;
+           } else {
+             console.log("cant get individual graph");
+           }
+         }
+       }
+     } else {
+       console.log("cant get graphCol");
+     }
+  });
+});
+
+var config = { childList: true, subtree: true };
+observer.observe(document.body, config); // Observing changes in the entire document
 
 function getStatsList() {
   var statsList = [];
-  var statsColEl = document.querySelector("#room-teambuilder > div > div.teambuilder-results > div.statform > div.col.statscol")
+  var statsColEl = document.querySelector("#room-teambuilder > div > div.teambuilder-results > div.statform > div.col.statscol");
   if(statsColEl) {
     for(var i = 2; i < 8; i++) {
       var statText = statsColEl.querySelector(`div:nth-child(${i}) > b`).innerText;
@@ -37,7 +39,6 @@ function getStatsList() {
         console.log("cant get individual stat");
       }
     }
-    console.log("stats: " + statsList);
     return statsList;
   } else {
     console.log("cant get statscol");
@@ -49,7 +50,6 @@ function getNewHueList(statsList) {
   for(var i = 0; i < statsList.length; i++) {
     hueList.push(statToHue(statsList[i]));
   }
-  console.log("hues: " + hueList);
   return hueList;
 }
 
